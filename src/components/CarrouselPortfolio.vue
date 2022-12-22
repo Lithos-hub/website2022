1d16864d9bf8ab475b6e432e2f02e31c94365aa4
Carlos Segura <lithos.contact@gmail.com>
2022-12-22 08:30:08 +0100

    improve web apps portfolio

--- src/components/CarrouselPortfolio.vue
+++ src/components/CarrouselPortfolio.vue
@@ -6,19 +6,26 @@
       :key="i"
       @click="goTo(element.link)"
     >
-      <h3>{{ element.name }}</h3>
-      <p>{{ element.subtitle }}</p>
-      <hr />
-      <h5>Tech stack</h5>
-      <section class="tech__stack--wrapper">
-        <div v-for="(tech, i) in element.tech_icons" :key="i">
-          <mdicon class="tech__icon" :name="tech.techicon" :size="20"></mdicon>
-          <p class="tech__name">{{ tech.techname }}</p>
-        </div>
-      </section>
-      <div class="card__image--wrapper">
-        <img class="card__image" :src="element.img" :alt="element.name" />
+      <div>
+        <h3>{{ element.name }}</h3>
+        <p>{{ element.subtitle }}</p>
+        <hr />
+        <h5>Tech stack</h5>
+
+        <article class="tech__stack--wrapper">
+          <div v-for="(tech, i) in element.tech_icons" :key="i">
+            <mdicon
+              class="tech__icon"
+              :name="tech.techicon"
+              :size="20"
+            ></mdicon>
+            <p class="tech__name">{{ tech.techname }}</p>
+          </div>
+        </article>
       </div>
+      <article class="card__image--wrapper">
+        <img class="card__image" :src="element.img" :alt="element.name" />
+      </article>
     </article>
   </section>
 </template>
@@ -115,7 +122,6 @@ onMounted(() => listenScrollX());
 
     .tech__stack--wrapper {
       position: relative;
-      top: 2vh;
       left: 0;
       transform: none;
       width: 100%;
@@ -172,17 +178,18 @@ onMounted(() => listenScrollX());
   }
   .card {
     transition: all 0.2s ease-in;
-    position: relative;
     border-top: 2px solid white;
     border-bottom: 2px solid cyan;
     border-inline: 2px solid transparent;
     color: $mainDark;
     min-width: clamp(100px, 500px, 280px);
     width: auto;
-
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     margin-bottom: 25px;
     border-radius: 25px;
-    padding: 25px;
+    padding: 10px;
 
     &:hover {
       border-radius: 20px;
@@ -216,13 +223,12 @@ onMounted(() => listenScrollX());
     }
 
     .tech__stack--wrapper {
-      width: 80%;
-      margin: 0 auto;
-      margin-top: 3rem;
+      width: 100%;
       height: auto;
       display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(20px, 1fr));
-      gap: 40px;
+      grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
+      gap: 20px;
+      margin-top: 40px;
 
       div {
         margin-bottom: 1em;
@@ -247,19 +253,13 @@ onMounted(() => listenScrollX());
       padding: 5px;
       border-radius: 7px;
     }
-    .card__image--wrapper {
-      position: absolute;
-      bottom: 20px;
-      left: 50%;
-      transform: translateX(-50%);
-    }
+
     .card__image {
       transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
-
       border-radius: 25px;
-      margin-top: 50px;
-      height: clamp(100px, 200px, 280px);
-      width: clamp(100px, 200px, 280px);
+      height: 200px;
+      width: 200px;
+      object-fit: cover;
     }
   }
 }
